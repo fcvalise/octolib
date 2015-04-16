ca87b91a3c525038dfb4934da398b7e46a6bf772
irabeson <irabeson@student.42.fr>
2015-04-16 02:46:47 +0200

    Added b-spline interpolation

--- src/Interpolations.cpp
+++ src/Interpolations.cpp
@@ -6,12 +6,12 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/14 20:00:37 by irabeson          #+#    #+#             */
-/*   Updated: 2015/03/23 01:16:04 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/04/16 03:00:48 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Interpolations.hpp"
-
+#include "Math.hpp"
 
 namespace octo
 {
@@ -34,6 +34,25 @@ namespace octo
 		result.g = cosinusInterpolation(a.g, b.g, alpha);
 		result.b = cosinusInterpolation(a.b, b.b, alpha);
 		result.a = cosinusInterpolation(a.a, b.a, alpha);
+		return (result);
+	}
+	  
+	sf::Vector2f	bsplineInterpolation(sf::Vector2f const& p0,
+										 sf::Vector2f const& p1,
+										 sf::Vector2f const& p2,
+										 sf::Vector2f const& p3,
+										 float t)
+	{
+		float const		CoefA = (1.f / 6.f);
+		float const 	CoefB = (3.f * octo::pow<3>(t) - 6.f * octo::pow<2>(t) + 4.f);
+		float const 	CoefC = (-3.f * octo::pow<3>(t) + 3.f * octo::pow<2>(t) + (3.f * t) + 1.f);
+		float const		CoefD = octo::pow<3>(t);
+		sf::Vector2f	result;
+
+		result.x =	CoefA * (octo::pow<3>(1.f - t) * p0.x +	(CoefB * p1.x) +
+						(CoefC * p2.x) + CoefD * p3.x);
+		result.y =	CoefA * (octo::pow<3>(1.f - t) * p0.y +	(CoefB * p1.y) +
+								(CoefC * p2.y) + CoefD * p3.y);
 		return (result);
 	}
 }
