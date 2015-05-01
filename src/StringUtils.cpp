e0b9009ef11e89197778db06324e5c4da6b44459
irabeson <irabeson@student.42.fr>
2015-05-01 18:05:56 +0200

    Code cleanup.
    fix #17

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/16 18:09:44 by irabeson          #+#    #+#             */
-/*   Updated: 2015/05/01 16:30:13 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/05/01 18:05:45 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ namespace octo
 		const std::ctype<wchar_t>&	ctfacet = std::use_facet<std::ctype<wchar_t>>(s_locale);
 		std::wstring				result;
 
+		result.reserve(str.size());
 		for (char c : str)
 			result.push_back(ctfacet.widen(c));
 		return (result);
@@ -33,6 +34,7 @@ namespace octo
 		const std::ctype<char>&	ctfacet = std::use_facet<std::ctype<char>>(s_locale);
 		std::string				result;
 
+		result.reserve(str.size());
 		for (wchar_t c : str)
 			result.push_back(ctfacet.narrow(c, '?'));
 		return (result);
