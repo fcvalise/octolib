396573ed2e186969b7b3bcad026a6f86fdd78205
irabeson <irabeson@student.42.fr>
2015-05-06 20:42:40 +0200

    Trying to fixes cppcheck build script

--- scripts/linux/install_cppcheck.sh
+++ scripts/linux/install_cppcheck.sh
@@ -2,7 +2,7 @@
 
 git clone git://github.com/danmar/cppcheck.git cppcheck-repository
 cd cppcheck-repository
-git pull origin master
-g++ -o cppcheck -std=c++0x -include lib/cxx11emu.h -DHAVE_RULES -Ilib -Iexternals/tinyxml cli/*.cpp lib/*.cpp externals/tinyxml/*.cpp -lpcre 
+echo "Building cppcheck..."
+g++ -o cppcheck -std=c++0x -include lib/cxx11emu.h -DCFGDIR="cfg" -DHAVE_RULES -Ilib -Iexternals/tinyxml cli/*.cpp lib/*.cpp externals/tinyxml/*.cpp -lpcre 
 mv cppcheck /usr/bin/
 cd ..
--- scripts/linux/run_static_analysis.sh
+++ scripts/linux/run_static_analysis.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export CFG_DIR="./cppcheck-repository/cfg/"
-
 cppcheck --error-exitcode=1 --force --std=c++11 -q --platform=unix64 -I /usr/local/include/ -I ./includes/ ./includes/ ./src/
 
 if [ "$?" != "0" ]; then
