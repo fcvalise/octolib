b19e7e2ecec8f0d1471e24d423fefb1e5500e970
irabeson <irabeson@student.42.fr>
2015-05-06 20:48:36 +0200

    Trying to fixes cppcheck build script

--- scripts/linux/install_cppcheck.sh
+++ scripts/linux/install_cppcheck.sh
@@ -3,6 +3,6 @@
 git clone git://github.com/danmar/cppcheck.git cppcheck-repository
 cd cppcheck-repository
 echo "Building cppcheck..."
-g++ -o cppcheck -std=c++0x -include lib/cxx11emu.h -DCFGDIR="cfg" -DHAVE_RULES -Ilib -Iexternals/tinyxml cli/*.cpp lib/*.cpp externals/tinyxml/*.cpp -lpcre 
-mv cppcheck /usr/bin/
+make SRCDIR=build CFGDIR=cfg HAVE_RULES=yes
+make install
 cd ..
