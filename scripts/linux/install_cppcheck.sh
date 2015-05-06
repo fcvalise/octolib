8031e89f40e1cbea726f214b1e1e6cee1bb80233
irabeson <irabeson@student.42.fr>
2015-05-06 20:08:02 +0200

    Fixes cppcheck install for linux

--- scripts/linux/install_cppcheck.sh
+++ scripts/linux/install_cppcheck.sh
@@ -3,6 +3,6 @@
 git clone git://github.com/danmar/cppcheck.git cppcheck-repository
 cd cppcheck-repository
 git pull origin master
-g++ -o cppcheck -std=c++0x -include lib/cxx11emu.h -lpcre -DHAVE_RULES -Ilib -Iexternals/tinyxml cli/*.cpp lib/*.cpp externals/tinyxml/*.cpp
+g++ -o cppcheck -std=c++0x -include lib/cxx11emu.h -DHAVE_RULES -Ilib -Iexternals/tinyxml cli/*.cpp lib/*.cpp externals/tinyxml/*.cpp -lpcre 
 mv cppcheck /usr/bin/
 cd ..
