8fc4a7e07dc6a10656617f7c0624274c5e2779e9
irabeson <irabeson@student.42.fr>
2015-05-07 16:41:31 +0200

    Re-trying again to fixes cppcheck rules problems

--- scripts/linux/install_cppcheck.sh
+++ scripts/linux/install_cppcheck.sh
@@ -3,6 +3,8 @@
 git clone git://github.com/danmar/cppcheck.git cppcheck-repository
 cd cppcheck-repository
 echo "Building cppcheck..."
+chmod 777 cfg
+chmod 777 cfg/*
 make SRCDIR=build CFGDIR=$PWD/cfg/ HAVERULE=yes
 make install
 cd ..
