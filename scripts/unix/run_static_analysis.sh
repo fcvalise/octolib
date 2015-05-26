eaeba3ae566a8f232b21d507ff164537d9ed5aed
irabeson <irabeson@student.42.fr>
2015-05-26 21:13:28 +0200

    Trying to fix static analysis script

--- scripts/unix/run_static_analysis.sh
+++ scripts/unix/run_static_analysis.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-cppcheck --inline-suppr --quiet --enable=all --error-exitcode=1 --force --std=c++11 -q --platform=unix64 -I/usr/local/include/ -I./includes/ -I./octolib/includes ./includes/ ./src/ 2>&1 | grep -e "warning" -e "error" > results
-cat results
-COUNT="$(wc -l results)"
-if [ "$COUNT" != "0" ]; then
-	exit 666
+cppcheck --inline-suppr --quiet --enable=all --error-exitcode=1 --force --std=c++11 -q --platform=unix64 -I/usr/local/include/ -I./includes/ -I./octolib/includes ./includes/ ./src/ 2>&1 | grep -e "warning" -e "error" > cppcheck_results.txt
+if [ -s cppcheck_results.txt ]; then
+	cat cppcheck_results.txt
+	exit 1
 fi
