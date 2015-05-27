66af07ffd639783283483b4f762192dabe1a5c2e
irabeson <irabeson@student.42.fr>
2015-05-27 18:13:41 +0200

    Added ConsoleAutoComplete

--- includes/ConsoleAutoComplete.hpp
+++ includes/ConsoleAutoComplete.hpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/27 02:47:51 by irabeson          #+#    #+#             */
-/*   Updated: 2015/05/27 03:36:27 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/05/27 18:12:39 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 
 namespace octo
 {
+	class ConsoleAutoCompletion
+	{
+	public:
+
+	private:
+
+	};
+
 	class ConsoleAutoComplete
 	{
 		enum class	Lexems
@@ -30,9 +38,21 @@ namespace octo
 
 		typedef LexicalTree<wchar_t, Lexems, Lexems::Null, Lexems::Root>	Tree;
 	public:
-		void	addWord(std::wstring const& word, Lexems lexem);
+		typedef std::vector<std::wstring>				Completions;
+		typedef typename Completions::const_iterator	CompletionsIterator;
+
+		void					addWord(std::wstring const& word, Lexems lexem);
+		void					resetBuffer(std::wstring const& buffer);
+		bool					getCurrentCompletion(std::wstring& result);
+		bool					getNextCompletion(std::wstring& result);
+		bool					getPrevCompletion(std::wstring& result);
+		Completions const&		getCompletions()const;
+		bool					hasCompletions()const;
 	private:
-		Tree	m_tree;
+		Tree				m_tree;
+		Completions			m_completions;
+		CompletionsIterator	m_current;
+		std::wstring		m_buffer;
 	};
 }
 
--- src/ConsoleAutoComplete.cpp
+++ src/ConsoleAutoComplete.cpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/27 03:15:35 by irabeson          #+#    #+#             */
-/*   Updated: 2015/05/27 03:17:26 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/05/27 18:13:09 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,64 @@ namespace octo
 	void	ConsoleAutoComplete::addWord(std::wstring const& word, Lexems lexem)
 	{
 		m_tree.addWord(word, lexem);
+	}
+	
+	void	ConsoleAutoComplete::resetBuffer(std::wstring const& buffer)
+	{
+		if (m_buffer != buffer)
+		{
+			m_completions.clear();
+			m_tree.completeWord(buffer, m_completions);
+			m_current = m_completions.begin();
+			m_buffer = buffer;
+		}
+	}
+
+	bool	ConsoleAutoComplete::getCurrentCompletion(std::wstring& result)
+	{
+		bool	hasResult = false;
+
+		if (m_current != m_completions.end())
+		{
+			result.assign(*m_current);
+			hasResult = true;
+		}
+		return (hasResult);
+	}
+
+	bool	ConsoleAutoComplete::getNextCompletion(std::wstring & result)
+	{
+		bool	hasResult = false;
+
+		if (m_current != m_completions.end() && m_current + 1 != m_completions.end())
+		{
+			++m_current;
+			result.assign(*m_current);
+			hasResult = true;
+		}
+		return (hasResult);
+	}
+
+	bool	ConsoleAutoComplete::getPrevCompletion(std::wstring & buffer)
+	{
+		bool	hasResult = false;
+
+		if (m_current != m_completions.end() && m_current - 1 != m_completions.end())
+		{
+			--m_current;
+			buffer.assign(*m_current);
+			hasResult = true;
+		}
+		return (hasResult);
+	}
+	
+	ConsoleAutoComplete::Completions const&		ConsoleAutoComplete::getCompletions()const
+	{
+		return (m_completions);
+	}
+
+	bool		ConsoleAutoComplete::hasCompletions()const
+	{
+		return (m_completions.empty() == false);
 	}
 }
