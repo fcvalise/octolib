fcvalise/octolib

commit bb972697a13587db6bf1c132a7e9fdedd506e174

Added constructors requiered by g++

includes/PackageReader.hpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/25 02:51:53 by irabeson          #+#    #+#             */
-/*   Updated: 2015/04/01 11:27:05 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/05/30 21:54:27 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,9 @@ namespace octo
 	class PackageReader
 	{
 	public:
+		PackageReader() = default;
+		PackageReader(PackageReader&& other);
+
 		/*!	Open a package file */
 		bool					open(std::string const& fileName);
 

src/PackageReader.cpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/25 07:03:10 by irabeson          #+#    #+#             */
-/*   Updated: 2015/04/01 11:28:07 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/05/30 21:54:05 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 
 namespace octo
 {
+	PackageReader::PackageReader(PackageReader&& other) :
+		m_header(std::move(other.m_header)),
+		m_file(std::move(other.m_file)),
+		m_encryptionMask(std::move(m_encryptionMask))
+	{
+	}
+
 	bool	PackageReader::open(std::string const& fileName)
 	{
 		m_file.open(fileName.c_str(), std::ios_base::binary);

commit 701f454900126301320855786894650af2addb9d

Fixe crash on delayed resources loading introduced by bb972697a13587db6bf1c132a7e9fdedd506e174

includes/PackageReader.hpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/25 02:51:53 by irabeson          #+#    #+#             */
-/*   Updated: 2015/05/30 23:44:57 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/06/06 06:03:16 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ namespace octo
 	{
 	public:
 		PackageReader();
-		PackageReader(PackageReader&& other);
 
 		/*!	Open a package file */
 		bool					open(std::string const& fileName);

src/PackageReader.cpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/25 07:03:10 by irabeson          #+#    #+#             */
-/*   Updated: 2015/05/30 23:45:42 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/06/06 06:03:25 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,6 @@ namespace octo
 {
 	PackageReader::PackageReader() :
 		m_file(new std::ifstream)
-	{
-	}
-
-	PackageReader::PackageReader(PackageReader&& other) :
-		m_header(std::move(other.m_header)),
-		m_file(std::move(other.m_file)),
-		m_encryptionMask(std::move(m_encryptionMask))
 	{
 	}
 