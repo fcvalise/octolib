b00dd54924aa7d1ccc23101e1d77fdc353244712
irabeson <irabeson@student.42.fr>
2015-06-12 13:03:23 +0200

    More efficient static analysis

--- scripts/unix/run_static_analysis.sh
+++ scripts/unix/run_static_analysis.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-cppcheck --inline-suppr --quiet --enable=all --error-exitcode=1 --force --std=c++11 -q --platform=unix64 -I/usr/local/include/ -I./includes/ -I./octolib/includes ./includes/ ./src/ 2>&1 | grep -e "warning" -e "error" > cppcheck_results.txt
+INCLUDES="./includes/* ./includes/details/*"
+SRCS="./src/"
+
+cppcheck --inline-suppr --quiet --enable=all --error-exitcode=1												\
+		 --force --std=c++11 -q --platform=unix64 --suppressions ./scripts/unix/cpp_check_suppressions.txt 	\
+		 -I/usr/local/include/ $INCLUDES $SRCS 2>&1 | grep -e "warning" -e "error" > cppcheck_results.txt
+
 if [ -s cppcheck_results.txt ]; then
 	cat cppcheck_results.txt
+	rm cppcheck_results.txt
 	exit 1
+else
+	rm cppcheck_results.txt
+	exit 0
 fi
