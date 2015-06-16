8e29defca36ad78686ad4ef6b6242a22ba5ad06d
irabeson <irabeson@student.42.fr>
2015-06-17 01:39:33 +0200

    Cleanup of travis - reducing boost setup to boost-test only

--- scripts/unix/install_dependencies.sh
+++ scripts/unix/install_dependencies.sh
@@ -2,31 +2,8 @@
 
 set -e
 
-echo "deb http://llvm.org/apt/precise/ llvm-toolchain-precise main" | tee -a /etc/apt/sources.list
-echo "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main" | tee -a /etc/apt/sources.list
-echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu precise main" | tee -a /etc/apt/sources.list
-apt-get update -qq
-
 packages="
-libpthread-stubs0-dev
-libx11-dev
-libgl1-mesa-dev
-libxrandr-dev
-libfreetype6-dev
-libglew1.5-dev
-libjpeg8-dev
-libsndfile1-dev
-libopenal-dev
-libudev-dev
-libx11-xcb-dev
-libxcb-icccm4-dev
-libxcb-image0-dev
-libxcb-util0-dev
-libxcb-randr0-dev
 libboost-test-dev
-libpcre3-dev
-cmake
-clang-3.5
 "
 
 for package in $packages
