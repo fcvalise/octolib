39f1237d39204ec2633c8aab847e34fc117764c3
irabeson <irabeson@student.42.fr>
2015-06-18 20:45:12 +0200

    Camera cleanup
    fix #94

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/07 20:30:10 by irabeson          #+#    #+#             */
-/*   Updated: 2015/06/06 05:16:24 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/06/18 20:44:25 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 
 namespace octo
 {
+	/*!
+	 *	\ingroup Graphics
+	 *	\class Camera
+	 *	\brief Camera
+	 *
+	 *	Camera provides unified view controls.
+	 *	\see Application
+	 */
+
+	/*!	Create a null camera */
 	Camera::Camera() :
 		m_graphicsManager(nullptr),
 		m_zoomFactor(1.f)
@@ -32,76 +42,95 @@ namespace octo
 		m_zoomFactor = 1.f;
 	}
 
+	/*!	Defines the center of the camera */
 	void	Camera::setCenter(sf::Vector2f const& center)
 	{
 		m_view.setCenter(center);
 	}
 
+	/*!	Defines the center of the camera */
 	void	Camera::setCenter(float x, float y)
 	{
 		m_view.setCenter(x, y);
 	}
 
+	/*!	Moves the center of the camera */
 	void	Camera::move(sf::Vector2f const& offset)
 	{
 		m_view.move(offset);
 	}
 
+	/*!	Moves the center of the camera */
 	void	Camera::move(float offsetX, float offsetY)
 	{
 		m_view.move(offsetX, offsetY);
 	}
 
+	/*!	Defines the camera rotation in degrees
+	 *	\param angle Angle in degrees
+	 */
 	void	Camera::setRotation(float angle)
 	{
 		m_view.setRotation(angle);
 	}
 
+	/*!	Rotate the camera
+	 *	\param angle Angle in degrees
+	 */
 	void	Camera::rotate(float angle)
 	{
 		m_view.rotate(angle);
 	}
 
+	/*!	Define the zoom factor */
 	void	Camera::setZoom(float factor)
 	{
 		m_zoomFactor = factor;
 		m_view.zoom(m_zoomFactor);
 	}
 
+	/*!	Get the position of the center of the camera */
 	sf::Vector2f const&	Camera::getCenter()const
 	{
 		return (m_view.getCenter());
 	}
 
+	/*!	Get the size of the camera */
 	sf::Vector2f const&	Camera::getSize()const
 	{
 		return (m_view.getSize());
 	}
 
+	/*!	Get the camera rotation */
 	float	Camera::getRotation()const
 	{
 		return (m_view.getRotation());
 	}
 
+	/*!	Get the zoom factor */
 	float	Camera::getZoom()const
 	{
 		return (m_zoomFactor);
 	}
 
+	/*! Update the camera */
 	void	Camera::update(sf::Time)
 	{
 	}
 
+	/*!	Get the SFML view handled by the camera */
 	sf::View const&	Camera::getView()const
 	{
 		return (m_view);
 	}
 
+	/*!	Get the SFML view dedicated to the gui handled by the camera */
 	sf::View const&	Camera::getGuiView()const
 	{
 		return (m_guiView);
 	}
 
+	/*!	Get the camera rectangle */
 	sf::FloatRect	Camera::getRectangle()const
 	{
 		sf::Vector2f	size = m_view.getSize();
@@ -109,6 +138,7 @@ namespace octo
 		return (sf::FloatRect(m_view.getCenter() - size / 2.f, size));
 	}
 
+	/*!	Convert a pixel position on the screen to a position in world coordinate using the game view */
 	sf::Vector2f	Camera::mapPixelToCoords(sf::Vector2i const& position)const
 	{
 		assert (m_graphicsManager != nullptr);
@@ -116,6 +146,7 @@ namespace octo
 		return (m_graphicsManager->mapPixelToCoords(position, m_view));
 	}
 
+	/*!	Convert a pixel position on the screen to a position in world coordinate using the GUI view */
 	sf::Vector2f	Camera::mapGuiPixelToCoords(sf::Vector2i const& position)const
 	{
 		assert (m_graphicsManager != nullptr);
@@ -123,6 +154,7 @@ namespace octo
 		return (m_graphicsManager->mapPixelToCoords(position, m_guiView));
 	}
 
+	/*!	Convert a position in world coordinate to a pixel position on the screen using the game view */
 	sf::Vector2i	Camera::mapCoordsToPixel(sf::Vector2f const& position)const
 	{
 		assert (m_graphicsManager != nullptr);
@@ -130,6 +162,7 @@ namespace octo
 		return (m_graphicsManager->mapCoordsToPixel(position, m_view));
 	}
 
+	/*!	Convert a position in world coordinate to a pixel position on the screen using the GUI view */
 	sf::Vector2i	Camera::mapGuiCoordsToPixel(sf::Vector2f const& position)const
 	{
 		assert (m_graphicsManager != nullptr);
