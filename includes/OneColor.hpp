0bfe5f39d9be230d6d4ec28af4b050514eddf84e
irabeson <irabeson@student.42.fr>
2015-06-22 17:42:39 +0200

    Documentation and minor fixes

--- includes/OneColor.hpp
+++ includes/OneColor.hpp
@@ -6,7 +6,7 @@
 /*   By: irabeson <irabeson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/21 22:29:26 by irabeson          #+#    #+#             */
-/*   Updated: 2015/06/21 22:40:43 by irabeson         ###   ########.fr       */
+/*   Updated: 2015/06/22 17:42:10 by irabeson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@
 
 namespace octo
 {
+	/*!	\ingroup Graphics
+	 *	\class OneColor
+	 *	\brief Provider which can gives only one color
+	 */
 	class OneColor : public IColorProvider
 	{
 	public:
 		explicit OneColor(sf::Color const& color);
 
 		void				setColor(sf::Color const& color);
-		virtual sf::Color	getColor(std::size_t id = 0u)const;
+		virtual sf::Color	getColor(std::size_t id)const;
 		virtual std::size_t	getColorCount()const;
 	private:
 		sf::Color	m_color;
