a3adcd977eebffd2ba12e53d7b2b6f9843857643
irabeson <irabeson@student.42.fr>
2015-08-11 00:50:13 +0200

    Removed shitty path passed to cppcheck

--- scripts/travis/run_static_analysis.sh
+++ scripts/travis/run_static_analysis.sh
@@ -2,7 +2,7 @@
 
 cppcheck --inline-suppr --quiet --enable=all --error-exitcode=1 --force --std=c++11 -q --platform=unix64 	\
 		 --suppressions ./scripts/travis/cppcheck_suppressions.txt											\
-			-I/usr/local/include/ -I./includes/ -I./octolib/includes ./includes/ ./src/ 					\
+		 -I/usr/local/include/ -I./includes/ ./includes/ ./src/ 											\
 			2>&1 | grep -e "warning" -e "error" > cppcheck_results.txt
 
 if [ -s cppcheck_results.txt ]; then
